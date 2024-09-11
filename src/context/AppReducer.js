67cbafadc89b59572912efa45fb3320726142902
Karthik-GR <karthikgr525@gmail.com>
2024-09-11 18:02:10 +0530

    first commit

---
+++ src/context/AppReducer.js
@@ -0,0 +1,15 @@
+import Transaction from "../components/Transaction";
+
+export default (state, action) => {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (Transaction) => Transaction.id !== action.payload
+        ),
+      };
+    default:
+      return state;
+  }
+};
