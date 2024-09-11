73671087d063bd31a291987bb7caf8047fa10469
Karthik-GR <karthikgr525@gmail.com>
2024-09-11 18:27:05 +0530

    react component without database

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -6,8 +6,13 @@ export default (state, action) => {
       return {
         ...state,
         transactions: state.transactions.filter(
-          (Transaction) => Transaction.id !== action.payload
+          (transaction) => transaction.id !== action.payload
         ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
       };
     default:
       return state;
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,12 +2,7 @@ import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [],
 };
 
 export const GlobalContext = createContext(initialState);
@@ -23,9 +18,19 @@ export const GlobalProvider = ({ children }) => {
       payload: id,
     });
   }
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
   return (
     <GlobalContext.Provider
-      value={{ transactions: state.transactions, deleteTransaction }}
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
     >
       {children}
     </GlobalContext.Provider>
